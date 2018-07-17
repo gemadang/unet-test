af19add9cad02c637da1ed2932c8691c1cf2b3e9
Geri Madanguit <gemadang@syr.edu>
2018-07-17 09:38:57 -0700

    just the model

--- run.sh
+++ run.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
-APT_PACKAGES="apt-utils ffmpeg libav-tools x264 x265"
-apt-install() {
-	export DEBIAN_FRONTEND=noninteractive
-	apt-get update -q
-	apt-get install -q -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" $APT_PACKAGES
-	return $?
-}
+#APT_PACKAGES="apt-utils ffmpeg libav-tools x264 x265"
+#apt-install() {
+#	export DEBIAN_FRONTEND=noninteractive
+#	apt-get update -q
+#	apt-get install -q -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" $APT_PACKAGES
+#	return $?
+#}
 
 #install ffmpeg to container
-add-apt-repository -y ppa:jonathonf/ffmpeg-3 2>&1
-apt-install || exit 1
+#add-apt-repository -y ppa:jonathonf/ffmpeg-3 2>&1
+#apt-install || exit 1
 
-#run style transfer on video
 python model.py
