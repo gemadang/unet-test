9434aea8e820094a4661004f6dde3a9d66857ac1
Geri Madanguit <gemadang@syr.edu>
2018-07-18 13:56:24 -0700

    fixed run.sh

--- run.sh
+++ run.sh
@@ -21,6 +21,7 @@ pip install numpy
 pip install awscli --upgrade --ignore-installed six
 
 aws s3 sync s3://japan-roof-top-bucket /storage --no-sign-request
-ls -la /storage
+ls -la /storage/Train_label
+ls -la /storage/Train_data
 
-python model.py
+python model2.py
